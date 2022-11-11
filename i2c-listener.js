8f70d6223fa253561e313d16ec64be2365247d36
Patrik Pancisin <pancisin.patrik@hotmail.com>
2022-11-11 12:58:44 +0100

    initial commit

---
+++ i2c-listener.js
@@ -0,0 +1,35 @@
+module.exports = function (RED) {
+  "use strict";
+  const i2cBus = require("./i2c-bus");
+  const bus = i2cBus(1);
+
+  bus.start();
+
+  function I2CListenerNode(n) {
+    RED.nodes.createNode(this, n);
+
+    this.stack = parseInt(n.stack, 10);
+    this.channel = parseInt(n.channel, 10);
+    this.getPayload = (positive) => {
+      if (positive) {
+        return n.send || 1;
+      }
+
+      return 0;
+    };
+    var node = this;
+
+    bus.register(this.stack, this.channel, (value) => {
+      if (node.value !== value) {
+        node.send({
+          payload: node.getPayload(value !== 0),
+          channel: node.channel,
+          stack: node.stack,
+        });
+        node.value = value;
+      }
+    });
+  }
+
+  RED.nodes.registerType("i2clistener", I2CListenerNode);
+};
